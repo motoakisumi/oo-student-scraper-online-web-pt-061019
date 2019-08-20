5610e7486612c9a50a3097e0aab8bf2effd4f011
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-20 04:49:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    binding.pry
-    student_site = Nokogiri::HTML(open(index_url))
 
+    student_site = Nokogiri::HTML(open(index_url))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
