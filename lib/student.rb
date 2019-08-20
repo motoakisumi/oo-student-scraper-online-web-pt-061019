a7ed8bc4c54148350f08e14757e633f3e4d0884d
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-20 07:07:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+      @twitter = attributes_hash[:twitter]
+      @linkedin = attributes_hash[:linkedin]
+      @github = attributes_hash[:github]
+      @blog = attributes_hash[:blog]
+      @profile_quote = attributes_hash[:profile_quote]
+      @bio = attributes_hash[:bio]
+      @profile_url = attributes_hash[:profile_url]
   end
 
   def self.all
